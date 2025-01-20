d3bd78412ca8898ba233b14e7616380f4eef4dca
IgorMarinhoArgollo <igormarinhoargollo@gmail.com>
2025-01-20 13:06:15 -0300

    ready

--- client.py
+++ client.py
@@ -26,8 +26,8 @@ def autenticar(client: socket) -> str:
         option = input(welcome_message).lower()
 
         if option != "login" and option != "cadastro":
-            logging.warning("Opção inválida. Tente novamente.")
-            continue
+          logging.warning("Opção inválida. Tente novamente.")
+          continue
         
         user = input("Digite seu usuário: ").strip()
         password = input("Digite sua senha: ").strip()
@@ -36,37 +36,34 @@ def autenticar(client: socket) -> str:
         encrypted_message = encrypt_message(message)
         client.send(encrypted_message.encode('utf-8'))
 
-
         encrypted_response = client.recv(1024).decode('utf-8') # Possiveis respostas: USERTAKEN e SUCCESS
         response = decrypt_message(encrypted_response)
 
         # --- Respostas para CADASTRO ---
         if response == "USERTAKEN":
-            logging.error("Usuário já existe! Tente novamente") 
-            continue
+          logging.error("Usuário já existe! Tente novamente") 
+          continue
 
         elif response == "SUCCESSCAD":
-            logging.info("Usuário cadastrado com sucesso!")
-            continue
+          logging.info("Usuário cadastrado com sucesso!")
+          continue
         
         # --- Respostas para LOGIN ---
         elif response == "INVALIDCREDENTIALS":
-            logging.error("Credenciais inválidas! Tente novamente")
-            continue
+          logging.error("Credenciais inválidas! Tente novamente")
+          continue
 
         elif response == "SUCCESSLOG":
-            logging.info("Usuário autenticado com sucesso!")
-            return user
+          logging.info("Usuário autenticado com sucesso!")
+          return user
 
         # --- DEFAULT ---
         else:
-            raise Exception("Resposta inesperada do servidor durante autenticação.")
+          raise Exception("Resposta inesperada do servidor durante autenticação.")
       
       except KeyboardInterrupt:
         client.close()
         break
-
-######################################################
 
 def receberMensagens(client: socket):
   while True:
@@ -76,32 +73,32 @@ def receberMensagens(client: socket):
       print("\n", message, "\n")
 
     except Exception as e:
-        logging.error(f"Erro ao receber mensagem: {e}")
-        client.close()
-        break
+      logging.error(f"Erro ao receber mensagem: {e}")
+      client.close()
+      break
 
-def receberArquivos(client: socket):
+def receberDados(client: socket):
   while True:
     try:
-        message = client.recv(1024).decode('utf-8')
-        try:
-          decrypted_message = decrypt_message(message)
-          print("\n", decrypted_message, "\n")
-        except:
-          print("\n", message, "\n")
+      message = client.recv(1024).decode('utf-8')
+      try:
+        decrypted_message = decrypt_message(message)
+        print("\n", decrypted_message, "\n")
+      except:
+        print("\n", message, "\n")
 
     except Exception as e:
-        logging.error(f"Erro ao receber mensagem: {e}")
-        client.close()
-        break
+      logging.error(f"Erro ao receber mensagem: {e}")
+      client.close()
+      break
 
 def enviarPrivada(client: socket, user: str):
   recipient = input("Digite o destinatário da mensagem: ").strip() 
   text = input("Digite a mensagem: ")
 
   if not text:
-     logging.error("A mensagem não pode ser vazia")
-     return
+    logging.error("A mensagem não pode ser vazia")
+    return
   
   message = f'privada-{user}-{recipient}-{text}'
   encrypted_message = encrypt_message(message)
@@ -112,16 +109,16 @@ def enviarPrivada(client: socket, user: str):
 
 
   if response == "SUCCESS":
-     logging.info("Mensagem enviada com sucesso")
+    logging.info("Mensagem enviada com sucesso")
   elif response == "INVALID":
-     logging.error("Destinatário não encontrado. Tente novamente")
+    logging.error("Destinatário não encontrado. Tente novamente")
 
 def enviarMulticast(client: socket, user: str):
   text = input("Digite a mensagem: ")
 
   if not text:
-     logging.error("A mensagem não pode ser vazia")
-     return
+    logging.error("A mensagem não pode ser vazia")
+    return
   
   message = f'multicast-{user}-{text}'
   encrypted_message = encrypt_message(message)
@@ -131,83 +128,76 @@ def enviarMulticast(client: socket, user: str):
   response = decrypt_message(encrypted_response) 
 
   if response == "SUCCESS":
-     logging.info("Mensagem enviada com sucesso")
+    logging.info("Mensagem enviada com sucesso")
   elif response == "FAIL":
-     logging.error("Erro ao enviar mensagem. Tente novamente")
+    logging.error("Erro ao enviar mensagem. Tente novamente")
 
 def enviarArquivo(client: socket, user:str):
   recipient = input("Digite o destinatário da mensagem: ").strip() 
   filename = input("Digite o caminho do arquivo: ").strip()
 
   if not os.path.exists(filename):
-     logging.error("O caminho para o arquivo não existe")
-     return
+    logging.error("O caminho para o arquivo não existe")
+    return
 
   with open(filename, "rb") as file:
-      file_name = filename.split("/")[-1]  # Extrai o nome
-      file_size = len(file.read())  # Tamanho do arquivo
-      file.seek(0)  # Reseta o arquivo para o inicio
+    file_name = filename.split("/")[-1]  # Extrai o nome
+    file_size = len(file.read())  # Tamanho do arquivo
+    file.seek(0)  # Reseta o arquivo para o inicio
 
-      # Envia o nome do arquivo e tamanho primeiro
-      decrypted_message = f'arquivo-{user}-{recipient}-{file_name}-{file_size}'
-      message=encrypt_message(decrypted_message)
+    # Envia o nome do arquivo e tamanho primeiro
+    decrypted_message = f'arquivo-{user}-{recipient}-{file_name}-{file_size}'
+    message=encrypt_message(decrypted_message)
 
-      client.send(message.encode("utf-8"))
-      response = client.recv(1024).decode('utf-8')
+    client.send(message.encode("utf-8"))
+    response = client.recv(1024).decode('utf-8')
 
-      """ if response != "READY":
-          logging.error("Server não está pronto para receber o arquivo")
-          return """
-
-      # Send the file in chunks
-      logging.info(f"Enviando arquivo {file_name}...")
-      with tqdm.tqdm(total=file_size, unit='B', unit_scale=True, desc=f"Sending {file_name}") as pbar:
-        chunk = file.read(1024)  # Envia 1KB por batch
-        while chunk:
-            client.send(chunk)
-            pbar.update(len(chunk))  # Barra de progresso
-            chunk = file.read(1024)
-      server_ack = client.recv(1024).decode('utf-8')
-      logging.info(server_ack)  
+    # Send the file in chunks
+    logging.info(f"Enviando arquivo {file_name}...")
+    with tqdm.tqdm(total=file_size, unit='B', unit_scale=True, desc=f"Sending {file_name}") as pbar:
+      chunk = file.read(1024)  # Envia 1KB por batch
+      while chunk:
+        client.send(chunk)
+        pbar.update(len(chunk))  # Barra de progresso
+        chunk = file.read(1024)
+    server_ack = client.recv(1024).decode('utf-8')
+    logging.info(server_ack)  
 
 def start_client():
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
+  client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  client.connect((HOST, PORT))
 
-    user = autenticar(client)
+  user = autenticar(client)
 
-    # Inicia thread para recebimento de mensagens 
-    threading.Thread(target=receberMensagens, args=(client,)).start() 
+  # Inicia thread para recebimento de arquivos
+  threading.Thread(target=receberDados, args=(client,)).start() 
 
-    # Inicia thread para recebimento de arquivos
-    threading.Thread(target=receberArquivos, args=(client,)).start() 
-
-    # Loop para escolha de tipo de mensagem, destinatário e input de mensagem
-    while True:
-      try:
-        option = input("Digite o tipo de mensagem a ser enviada ('privada', 'multicast', 'arquivo') ou 'sair': ").strip()
-        
-        # Processamento de envio de mensagens privadas
-        if option.lower() == 'privada':
-          enviarPrivada(client, user)
-            
-        # Processamento de envio de mensagens multicast
-        if option.lower() == 'multicast':
-          enviarMulticast(client, user)
-        
-        # Processamento de envio arquivo para destinatário único
-        if option.lower() == 'arquivo':
-          enviarArquivo(client, user)
-        
-        # Encerramento de conexão
-        if option.lower() == 'sair':
-          client.close()
-          return
-            
-      except Exception as e:
-        logging.error(e)
+  # Loop para escolha de tipo de mensagem, destinatário e input de mensagem
+  while True:
+    try:
+      option = input("Digite o tipo de mensagem a ser enviada ('privada', 'multicast', 'arquivo') ou 'sair': ").strip()
+      
+      # Processamento de envio de mensagens privadas
+      if option.lower() == 'privada':
+        enviarPrivada(client, user)
+          
+      # Processamento de envio de mensagens multicast
+      if option.lower() == 'multicast':
+        enviarMulticast(client, user)
+      
+      # Processamento de envio arquivo para destinatário único
+      if option.lower() == 'arquivo':
+        enviarArquivo(client, user)
+      
+      # Encerramento de conexão
+      if option.lower() == 'sair':
         client.close()
-        break
+        return
+          
+    except Exception as e:
+      logging.error(e)
+      client.close()
+      break
 
 if __name__ == "__main__":
-    start_client()
+  start_client()
--- server.py
+++ server.py
@@ -11,8 +11,8 @@ log_user = getpass.getuser()
 
 # Configurações de logging
 logging.basicConfig(
-    level=logging.INFO,
-    format=f"%(asctime)s [%(levelname)s] [{log_user}]: %(message)s",
+  level=logging.INFO,
+  format=f"%(asctime)s [%(levelname)s] [{log_user}]: %(message)s",
 )
 
 credenciais = {} # Salva as credenciais dos usuários cadastrados
@@ -26,46 +26,46 @@ NUMERO_DE_CONEXOES = 5
 def handle_auth(client_socket: socket, addr):
   while True:
     try:
-        encrypted_welcome = encrypt_message("Bem vindo! Digite 'login' ou 'cadastro': ")
-        client_socket.send(encrypted_welcome.encode('utf-8'))
+      encrypted_welcome = encrypt_message("Bem vindo! Digite 'login' ou 'cadastro': ")
+      client_socket.send(encrypted_welcome.encode('utf-8'))
 
-        try:
-          decrypted_message = decrypt_message(client_socket.recv(1024).decode('utf-8'))
-          option, username, password = decrypted_message.split("-") 
+      try:
+        decrypted_message = decrypt_message(client_socket.recv(1024).decode('utf-8'))
+        option, username, password = decrypted_message.split("-") 
 
-        except Exception as e: # Geralmente em caso de desconexao do client
-          logging.error("Erro ao receber opção de autenticação: {e}")
-          clients.remove(client_socket)
-          client_socket.close()
-          return
+      except Exception as e: # Geralmente em caso de desconexao do client
+        logging.error("Erro ao receber opção de autenticação: {e}")
+        clients.remove(client_socket)
+        client_socket.close()
+        return
 
-        if option == 'cadastro':
-            if username in credenciais:
-                encrypted_response = encrypt_message("USERTAKEN")
-                client_socket.send(encrypted_response.encode('utf-8'))
-                logging.info(f"Usuário {username} já possui cadastro")
-                continue
-            else:
-                credenciais[username] = password # Adiciona as credenciais à lista
-                logging.info(f"Novo usuário cadastrado: {username}")
-                encrypted_response = encrypt_message("SUCCESSCAD") 
-                client_socket.send(encrypted_response.encode('utf-8'))
-                continue
-
-        elif option == 'login':
-            if username in credenciais and credenciais[username] == password:
-                logging.info(f"Novo usuário autenticado: {username}")
-                authenticated_clients[username] = client_socket
-                encrypted_response = encrypt_message("SUCCESSLOG")
-                client_socket.send(encrypted_response.encode('utf-8'))
-                handle_client(client_socket, username)
-            else:
-                encrypted_response = encrypt_message("INVALIDCREDENTIALS")
-                client_socket.send(encrypted_response.encode('utf-8'))
-        
-        else:
-          encrypted_response = encrypt_message("Invalid option. Disconnecting.")
+      if option == 'cadastro':
+        if username in credenciais:
+          encrypted_response = encrypt_message("USERTAKEN")
           client_socket.send(encrypted_response.encode('utf-8'))
+          logging.info(f"Usuário {username} já possui cadastro")
+          continue
+        else:
+          credenciais[username] = password # Adiciona as credenciais à lista
+          logging.info(f"Novo usuário cadastrado: {username}")
+          encrypted_response = encrypt_message("SUCCESSCAD") 
+          client_socket.send(encrypted_response.encode('utf-8'))
+          continue
+
+      elif option == 'login':
+        if username in credenciais and credenciais[username] == password:
+          logging.info(f"Novo usuário autenticado: {username}")
+          authenticated_clients[username] = client_socket
+          encrypted_response = encrypt_message("SUCCESSLOG")
+          client_socket.send(encrypted_response.encode('utf-8'))
+          handle_client(client_socket, username)
+        else:
+          encrypted_response = encrypt_message("INVALIDCREDENTIALS")
+          client_socket.send(encrypted_response.encode('utf-8'))
+      
+      else:
+        encrypted_response = encrypt_message("Invalid option. Disconnecting.")
+        client_socket.send(encrypted_response.encode('utf-8'))
     
     except BrokenPipeError:
       logging.warning(f"Perda de conexão com client: {addr}")
@@ -76,113 +76,106 @@ def handle_auth(client_socket: socket, addr):
     except Exception as e:
       print(f"Error: {e}")
 
-######################################################################################3
-
 def handle_client(client_socket: socket, username: str):
   while True:
     try:
-        encrypted_message = client_socket.recv(1024).decode('utf-8')
-        whole_message = decrypt_message(encrypted_message)
-        message = whole_message.split("-")
+      encrypted_message = client_socket.recv(1024).decode('utf-8')
+      whole_message = decrypt_message(encrypted_message)
+      message = whole_message.split("-")
 
-        if message[0] == "privada":
-          sender, recipient, text = message[1:]
+      if message[0] == "privada":
+        sender, recipient, text = message[1:]
 
-          if recipient in authenticated_clients:
-            recipient_socket = authenticated_clients[recipient]
-            encrypted_message = encrypt_message(f"{sender}: {text}")
-            recipient_socket.send(encrypted_message.encode('utf-8'))
-            encrypted_message = encrypt_message("SUCCESS")
-            client_socket.send(encrypted_message.encode('utf-8'))
-            logging.info(f"Mensagem privada enviada de {sender} para {recipient} com sucesso")
-          else:
-            encrypted_message = encrypt_message("INVALID")
-            client_socket.send(encrypted_message.encode('utf-8'))
-        
-        if message[0] == "multicast":
-          sender, text = message[1:]
+        if recipient in authenticated_clients:
+          recipient_socket = authenticated_clients[recipient]
+          encrypted_message = encrypt_message(f"{sender}: {text}")
+          recipient_socket.send(encrypted_message.encode('utf-8'))
+          encrypted_message = encrypt_message("SUCCESS")
+          client_socket.send(encrypted_message.encode('utf-8'))
+          logging.info(f"Mensagem privada enviada de {sender} para {recipient} com sucesso")
+        else:
+          encrypted_message = encrypt_message("INVALID")
+          client_socket.send(encrypted_message.encode('utf-8'))
+      
+      if message[0] == "multicast":
+        sender, text = message[1:]
 
-          for user in authenticated_clients:
-            _success = True # Controla se todos os usuários receberam a mensagem
-            if user != sender:
-              try:
-                recipient_socket = authenticated_clients[user]
-                encrypted_message = encrypt_message(f"{sender}: {text}")
-                recipient_socket.send(encrypted_message.encode('utf-8'))
-              except:
-                logging.error(f"Falha ao enviar multicast para user: {user}")
-                _success = False
+        for user in authenticated_clients:
+          _success = True # Controla se todos os usuários receberam a mensagem
+          if user != sender:
+            try:
+              recipient_socket = authenticated_clients[user]
+              encrypted_message = encrypt_message(f"{sender}: {text}")
+              recipient_socket.send(encrypted_message.encode('utf-8'))
+            except:
+              logging.error(f"Falha ao enviar multicast para user: {user}")
+              _success = False
 
-          if _success:
-            encrypted_message = encrypt_message("SUCCESS")
-            client_socket.send(encrypted_message.encode('utf-8'))
-            logging.info(f"Mensagem multicast de {sender} enviada com sucesso")
-          else:
-            encrypted_message = encrypt_message("FAIL")
-            client_socket.send(encrypted_message.encode('utf-8'))
+        if _success:
+          encrypted_message = encrypt_message("SUCCESS")
+          client_socket.send(encrypted_message.encode('utf-8'))
+          logging.info(f"Mensagem multicast de {sender} enviada com sucesso")
+        else:
+          encrypted_message = encrypt_message("FAIL")
+          client_socket.send(encrypted_message.encode('utf-8'))
 
-        if message[0] == "arquivo":
-          sender, recipient, filename, file_size = message[1:]
-          file_size = int(file_size)
+      if message[0] == "arquivo":
+        sender, recipient, filename, file_size = message[1:]
+        file_size = int(file_size)
 
-          if recipient in authenticated_clients:
-            client_socket.send(b"READY")
+        if recipient in authenticated_clients:
+          client_socket.send(b"READY")
 
-            # Open a file to save the incoming data
-            with open(f"received_{filename}", "wb") as file:
-                logging.info(f"Recebendo arquivo {filename} de {sender} ...")
-                received = 0
-                with tqdm.tqdm(total=file_size, unit='B', unit_scale=True, desc=f"Receiving {filename}") as pbar:
-                  while received < file_size:
-                      file_data = client_socket.recv(1024)
-                      file.write(file_data)
-                      received += len(file_data)
-                      pbar.update(len(file_data))
+          with open(f"received_{filename}", "wb") as file:
+              logging.info(f"Recebendo arquivo {filename} de {sender} ...")
+              received = 0
+              with tqdm.tqdm(total=file_size, unit='B', unit_scale=True, desc=f"Receiving {filename}") as pbar:
+                while received < file_size:
+                    file_data = client_socket.recv(1024)
+                    file.write(file_data)
+                    received += len(file_data)
+                    pbar.update(len(file_data))
 
-            # Send acknowledgment after the file is received
-            client_socket.send(b"File received successfully.")
-            logging.info(f"File {filename} received successfully.")
+          client_socket.send(b"File received successfully.")
+          logging.info(f"File {filename} received successfully.")
 
-            # recipient_socket = authenticated_clients[recipient]
-            # recipient_socket.send(f"{sender}-{file_name}-{file_size}".encode('utf-8'))
-          else:
-            client_socket.send(f"Destinatário não está autenticado".encode("utf-8"))
+        else:
+          client_socket.send(f"Destinatário não está autenticado".encode("utf-8"))
 
     except Exception as e:
       logging.error(e)
       break
 
 def start_server():
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PORT))
-    server.listen(5)
-    logging.info(f"Server ouvindo em {HOST}:{PORT}")
-    logging.info(f"Permitindo {NUMERO_DE_CONEXOES} conexões")
+  server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  server.bind((HOST, PORT))
+  server.listen(5)
+  logging.info(f"Server ouvindo em {HOST}:{PORT}")
+  logging.info(f"Permitindo {NUMERO_DE_CONEXOES} conexões")
 
-    try:
-      while True:
-          # Verifica conexões disponíveis
-          if len(clients) < NUMERO_DE_CONEXOES:
-            client_socket, addr = server.accept()
-            logging.info(f"Conexão aceita de {addr}")
-            clients.append(client_socket)
+  try:
+    while True:
+        # Verifica conexões disponíveis
+        if len(clients) < NUMERO_DE_CONEXOES:
+          client_socket, addr = server.accept()
+          logging.info(f"Conexão aceita de {addr}")
+          clients.append(client_socket)
 
-            client_handler = threading.Thread(target=handle_auth, args=(client_socket, addr))
-            client_handler.start()
-          else:
-            logging.warning(
-                "Limite de conexões atingido. Aguardando conexões livres."
-            )
-    except KeyboardInterrupt:
-        logging.info("Encerrando o servidor.")
-    finally:
-        server.close()
-
+          client_handler = threading.Thread(target=handle_auth, args=(client_socket, addr))
+          client_handler.start()
+        else:
+          logging.warning(
+              "Limite de conexões atingido. Aguardando conexões livres."
+          )
+  except KeyboardInterrupt:
+    logging.info("Encerrando o servidor.")
+  finally:
+    server.close()
 
 if __name__ == "__main__":
-    credenciais = {
-        'user1': '123',
-        'user2': '123',
-        'user3': '123'
-    }
-    start_server()
+  credenciais = {
+    'user1': '123',
+    'user2': '123',
+    'user3': '123'
+  }
+  start_server()
